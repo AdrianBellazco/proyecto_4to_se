5451a27d849f4df1df6e5f6b76260f100b231851
Adrian Saavedra <adrian007HD@gmail.com>
2024-05-05 21:12:21 -0500

    cambios

--- src/main/webapp/JS/main.js
+++ src/main/webapp/JS/main.js
@@ -80,41 +80,41 @@ var ScrollReveal=function(){"use strict";var r={delay:0,distance:"0",duration:60
 
 
 /*=============== SEARCH ===============*/
-/*const search = document.getElementById('search'),
+const search = document.getElementById('search'),
     searchBtn = document.getElementById('search-btn'),
     searchClose = document.getElementById('search-close')
 
 /* Search show */
-/*searchBtn.addEventListener('click', () =>{
+searchBtn.addEventListener('click', () =>{
     search.classList.add('show-search')
 })
 
 /* Search hidden */
-/*searchClose.addEventListener('click', () =>{
+searchClose.addEventListener('click', () =>{
     search.classList.remove('show-search')
 })
 
 /*=============== LOGIN ===============*/
-/*const login = document.getElementById('login'),
+const login = document.getElementById('login'),
     loginBtn = document.getElementById('login-btn'),
     loginClose = document.getElementById('login-close')
 
 /* Login show */
-/*loginBtn.addEventListener('click', () =>{
+loginBtn.addEventListener('click', () =>{
     login.classList.add('show-login')
 })
 
 /* Login hidden */
-/*loginClose.addEventListener('click', () =>{
+loginClose.addEventListener('click', () =>{
     login.classList.remove('show-login')
 })
 
 /*=============== RECORD ===============*/
-/*const registro = document.getElementById('record'),
+const registro = document.getElementById('record'),
     registroBtn = document.getElementById('record-btn'),
     registroClose = document.getElementById('record-close');
 
 /*=============== RECORD ===============*/
-/*const registro = document.getElementById('registro'),
+const registro = document.getElementById('registro'),
     registroBtn = document.getElementById('registro-btn'),
-    registroClose = document.getElementById('registro-close');*/
+    registroClose = document.getElementById('registro-close');
