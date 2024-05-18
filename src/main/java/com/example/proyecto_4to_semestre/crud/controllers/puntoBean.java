e4dbead3ee2560ce7743b655f7fd3b87e3b63e86
Adrian Saavedra <adrian007HD@gmail.com>
2024-05-18 18:37:32 -0500

    Se creo una clase para que me diera id unicos y se corrigio todos los problemas, podemos continuar

--- src/main/java/com/example/proyecto_4to_semestre/crud/controllers/puntoBean.java
+++ src/main/java/com/example/proyecto_4to_semestre/crud/controllers/puntoBean.java
@@ -7,8 +7,7 @@ import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Named("puntoBean")
 @SessionScoped
@@ -24,6 +23,9 @@ public class puntoBean implements Serializable {
     }
 
     public String createPunto(){
+
+
+        puntoVisitas.setIdpunto(generarCodigoUnico());
         puntoService.createpunto(puntoVisitas);
         this.setPuntoVisitas(new puntosVisitas());
         this.setListPuntos(puntoService.getAllpuntos());
@@ -31,7 +33,7 @@ public class puntoBean implements Serializable {
     }
 
     public String cancelarEdicion(){
-        this.puntoVisitas = new  puntosVisitas();
+        this.puntoVisitas = new puntosVisitas();
         return "system?faces-redirect=true";
     }
 
@@ -73,5 +75,32 @@ public class puntoBean implements Serializable {
 
     public void setPuntoVisitas(puntosVisitas puntoVisitas) {
         this.puntoVisitas = puntoVisitas;
+    }
+    public String generarCodigoUnico() {
+        Set<String> codigosExistentes = new HashSet<>();
+        for (puntosVisitas punto : ListPuntos) {
+            codigosExistentes.add(punto.getIdpunto());
+        }
+
+        String nuevoCodigo;
+        do {
+            nuevoCodigo = generarCodigo();
+        } while (codigosExistentes.contains(nuevoCodigo));
+
+        return nuevoCodigo;
+    }
+
+    // Generar un código aleatorio
+    public static String generarCodigo() {
+        Random random = new Random();
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numeros = "0123456789";
+
+        char letra1 = letras.charAt(random.nextInt(letras.length()));
+        char numero1 = numeros.charAt(random.nextInt(numeros.length()));
+        char letra2 = letras.charAt(random.nextInt(letras.length()));
+        char numero2 = numeros.charAt(random.nextInt(numeros.length()));
+
+        return "" + letra1 + numero1 + letra2 + numero2;
     }
 }
