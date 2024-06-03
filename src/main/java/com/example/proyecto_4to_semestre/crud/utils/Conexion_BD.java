670767427a55c4aa3e12b29f0ba31ba2b8b0eff1
KatherinPalacio <128763622+KatherinPalacio@users.noreply.github.com>
2024-06-03 18:10:08 -0500

    conn

--- src/main/java/com/example/proyecto_4to_semestre/crud/utils/Conexion_BD.java
+++ src/main/java/com/example/proyecto_4to_semestre/crud/utils/Conexion_BD.java
@@ -18,7 +18,7 @@ public class Conexion_BD {
             e.printStackTrace();
         }
         try {
-            return DriverManager.getConnection(url, username, "3187140146");
+            return DriverManager.getConnection(url, username, "Katherin2512");
         }catch (SQLException e){
             e.printStackTrace();
         }
